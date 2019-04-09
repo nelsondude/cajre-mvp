030e705cdd86001cdfbf951a2b3a08a48dcb20fd
nelsondude <alexn1336@gmail.com>
2019-04-09 14:37:28 -0400

    scaffold code

---
+++ src/components/Search/Search.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Search.css'
+
+class Search extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div className={"Search"}>
+        <h1>Search</h1>
+
+
+      </div>
+    )
+  }
+}
+
+export default Search;
